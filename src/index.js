24b27c84c8fefc5e493bfb3cf56ffa5d5e7e1e43
zayonvx <zayonvx@gmail.com>
2020-06-16 16:28:36 +0300

    test(components): add proptypes
    proptypes implemented
    in app and main
    
    МЕТА ДАННЫЕ: module2-task1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
 import App from './components/app/app.jsx';
+
+import {MOVIE_NAMES} from './utils/consts';
 
 const init = () => {
   const promo = {
@@ -15,6 +16,7 @@ const init = () => {
         promoTitle = {promo.title}
         promoGenre = {promo.genre}
         promoReleaseDate = {promo.releaseDate}
+        movieNames = {MOVIE_NAMES}
       />,
       document.querySelector(`#root`)
   );
